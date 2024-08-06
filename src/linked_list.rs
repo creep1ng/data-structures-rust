373a589633a79f32eaa4a635dfd1ee3a6a2d6b08
Ricardo Arias <reariast128@proton.me>
2024-08-05 22:08:56 -0500

    Implemented drop to deallocate unused nodes!

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -56,6 +56,22 @@ impl List {
     }
 }
 
+impl Drop for List {
+    fn drop(&mut self) {
+        // With `drop`, we tells to the compiler how to deallocate unused values from `List`.
+
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+
+        // `while let` == "do this thing until this pattern doesn't match"
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+            // boxed_node goes out of scope and gets dropped here;
+            // but its Node's `next` field has been set to Link::Empty
+            // so no unbounded recursion occurs.
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
