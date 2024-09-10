588914cc85f51469b019755e0115acb3b1cda4c7
Ricardo Arias <reariast128@proton.me>
2024-09-10 17:41:30 -0500

    Created the signature of the classify function. From now will be common the use of spanish.

---
+++ src/homework/knn.rs
@@ -0,0 +1,8 @@
+pub fn clasificar(conocidos: &[&[f64]], clases: &[i32], muestra: &f64, k: usize) -> Option<i32> {
+    if conocidos.len() != clases.len() {
+        return None;
+    }
+
+    // Have to implement the method.
+    Some(0)
+}
