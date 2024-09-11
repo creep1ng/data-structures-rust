33a90ad42a0b42fee222c386b99b4ebf733c38bb
Ricardo Arias <reariast128@proton.me>
2024-09-10 22:06:06 -0500

    Added cost analysis and worked on the implementation of the classify method.

--- src/homework/knn.rs
+++ src/homework/knn.rs
@@ -1,16 +1,37 @@
 use std::iter::zip;
 
-pub fn clasificar(conocidos: &[&[f64]], clases: &[i32], muestra: &f64, k: usize) -> Option<i32> {
-    if conocidos.len() != clases.len() {
-        return None;
+pub fn clasificar(conocidos: &[&[f64]], clases: &[i32], muestra: &[f64], k: usize) -> Option<i32> {
+    // All of these operations at this if statement are comparisons or memory-access operations, so we can assume that all this operation has a t1 cost.
+    if conocidos.len() != clases.len() // t1
+        || conocidos.is_empty() // + t2
+        || muestra.len() != conocidos[0].len()
+    // + t3
+    {
+        return None; // + t4
     }
 
-    // Have to implement the method.
+    // t5
+    // This is storing the index of the vector
+    let mut distances: Vec<(usize, f64)> = Vec::new();
+
+    // Since euclidean_distance has 7*O(1) + 4*O(N), having a growth order of N, this part of the code takes
+    for (index, vector) in conocidos.iter().enumerate() {
+        let distance = euclidean_distance(vector, muestra);
+        distances.push((index, distance.unwrap()))
+    }
+
+    // Here should occur the distance assign for each `conocidos` value, then sort them by distance, and select the most frequent from the bottom-k.
+
     Some(0)
 }
 
 /// Evaluates the euclidean distance between x and y.
+///
 /// Returns None if the vectors are not of the same length. Otherwise returns a f64.
+///
+/// This function has a runtime complexity equals to (t1+t2+t3+t9) + N(t4+t5+t6+t7+t8), and a growth order of N.
+///
+/// So, euclidean_distance~~O(N).
 fn euclidean_distance(x: &[f64], y: &[f64]) -> Option<f64> {
     // Both x.len() and y.len() haves constant time execution.
     // t1
@@ -22,11 +43,18 @@ fn euclidean_distance(x: &[f64], y: &[f64]) -> Option<f64> {
     // t2
     let mut squared_sum: f64 = 0.0;
 
-    // The loop iterates over each element of the size. Since the vector haves a known `n` dimension,
+    /*  zip creation is constant time (because it works by accessing to the iterators of the vectors).
+    the assignation for x_i and y_i are constant time (because they read a reference).
+    these assignations occurs n times, where n is the dimension of x and y vectors. */
+
+    //   N(t4 + t5)                t3
     for (x_i, y_i) in zip(x, y) {
+        // I'm not sure, but this seems that all these operations are constant time.
+        //          t6  +   t7  +   t8
         squared_sum += (y_i - x_i).powi(2);
     }
 
+    //              t9
     Some(squared_sum.sqrt())
 }
 
